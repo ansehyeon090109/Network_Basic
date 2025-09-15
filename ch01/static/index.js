ae55d54bf33446307275ddca6e5d5743320d8c55
ansehyeon090109 <ansehyeon09010@gmail.com>
2025-09-15 19:43:17 +0900

    1

--- ch01/static/index.js
+++ ch01/static/index.js
@@ -1,1 +1,9 @@
+var n = 0
+var num = document.getElementById("num")
+var plus = document.getElementById("plus")
+var forwarding = document.getElementById("forwarding")
 
+plus.addEventListener("click", function() {
+    n += 1
+    num.textContent = n
+})
